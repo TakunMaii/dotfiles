6eafb813ee9ef9ed34fe4a665e8d77f5c679c1a0
Maii on Catherim <maii_wzy@163.com>
2026-09-19 20:29:21 +0800

    delete unuseful files

--- .emacs.d/elpa/gruvbox-theme-20260911.539/gruvbox-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "gruvbox-theme" "20260911.539"
-  "A retro-groove colour theme for Emacs."
-  '((autothemer "0.2.19"))
-  :url "https://github.com/greduan/emacs-theme-gruvbox"
-  :commit "e629403d6f56f1483f86893b6a50f8d172d7c7a3"
-  :revdesc "e629403d6f56"
-  :authors '(("Jason Milkins" . "jasonm23@gmail.com"))
-  :maintainers '(("Jason Milkins" . "jasonm23@gmail.com")))
